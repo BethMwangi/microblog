6df69318776726589fe88bf47936e719b02eb476
BethMwangi <wanjikumwangib@gmail.com>
2016-05-07 00:55:15 +0300

    login template form added

--- app/forms.py
+++ app/forms.py
@@ -5,5 +5,5 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(Form):
-    openid = StringField('openid', validators = [DataRequired()])
+    openid = StringField('openid', validators = [DataRequired()]) #DataRequired validates the input to confirm user has submitted 
     remember_me = BooleanField('remember_me', default=False)
